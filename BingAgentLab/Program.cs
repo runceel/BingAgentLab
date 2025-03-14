63cd02de46803c1ee370f676f258163a6015c526
Kazuki Ota <kaota@microsoft.com>
2025-03-14 17:27:57 +0900

    プロジェクト ファイルを追加します。

---
+++ BingAgentLab/Program.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.Development.json", true)
+    .Build();
+
+var kernel = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(
+        configuration["AOAI:ModelDeploymentName"]!,
+        configuration["AOAI:Endpoint"]!,
+        new AzureCliCredential());
